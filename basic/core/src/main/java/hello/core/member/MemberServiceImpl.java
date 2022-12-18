6b3d81e3e0ea754ed4995d290336d08431ca87a0
ubs <ubs4939@naver.com>
2022-12-18 21:36:17 +0900

    basic_start

--- basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,6 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    // 의존관계가 인터페이스 뿐만아니라 구현체까지 의존하고 있음!! DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
--- basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ basic/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,9 +3,10 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepository implements MemberRepository {
+public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
+    //* 동시성 이슈 때문에 concurrent hashmap 써야함.
+    private static Map<Long,Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
