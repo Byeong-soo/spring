9c6ef3a46f785da92ce9c0bf67b09bdc57b6bd7e
ubs <ubs4939@naver.com>
2022-12-20 01:22:54 +0900

    TIL_2022_12_20

--- basic/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ basic/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,15 +1,19 @@
-package hello.core.Member;
+package hello.core.member;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
@@ -21,7 +25,5 @@ public class MemberServiceTest {
 
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
-
-
     }
 }
